58fa8894f461a00cf331cc8af6d2fe2e443cb996
Chris Rosenberger <jcrosenberger@pm.me>
2022-11-02 09:14:20 -0500

    Working on creating template modules

---
+++ evaluate.py
@@ -0,0 +1,286 @@
+# standard modules
+import seaborn as sns
+import pandas as pd
+import numpy as np
+import os
+#import math
+
+# Modules for Displaying Figures
+import matplotlib.pyplot as plt
+import scipy.stats as stats
+
+
+# Data Science Modules 
+from sklearn.tree import DecisionTreeClassifier, plot_tree, export_text
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report, confusion_matrix, plot_confusion_matrix, ConfusionMatrixDisplay
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+
+# My modules
+import src.acquire as ac
+import src.prepare as pp
+import src.helper as helper
+import src.evaluate as evaluate
+
+
+
+def get_data(): 
+    df = ac.get_telco_data()
+    return df 
+
+def print_classification_metrics(actuals, predictions):
+    '''A method which allows me to say time and coding space by making the metrics for evaluating 
+    a model simple and easy to access. Calculates Accuracy, Precision, Recall, Sensitivity and f1
+    '''    
+    
+    TN, FP, FN, TP = confusion_matrix(actuals, predictions).ravel()
+    ALL = TP + TN + FP + FN
+    
+    accuracy = (TP + TN)/ALL
+    print(f"Accuracy: {round(accuracy*100,2)}")
+
+    true_positive_rate = TP/(TP+FN)
+    print(f"True Positive Rate: {round(true_positive_rate*100,2)}")
+
+    false_positive_rate = FP/(FP+TN)
+    print(f"False Positive Rate: {round(false_positive_rate*100,2)}")
+
+    true_negative_rate = TN/(TN+FP)
+    print(f"True Negative Rate: {round(true_negative_rate*100,2)}")
+
+    false_negative_rate = FN/(FN+TP)
+    print(f"False Negative Rate: {round(false_negative_rate*100,2)}")
+
+    precision = TP/(TP+FP)
+    print(f"Precision: {round(precision*100,2)}")
+
+    recall = TP/(TP+FN)
+    print(f"Recall: {round(recall*100,2)}")
+
+    f1_score = 2*(precision*recall)/(precision+recall)
+    print(f"F1 Score: {round(f1_score*100,2)}")
+
+    support_pos = TP + FN
+    print(f"Support (0): {round(support_pos*100,2)}")
+
+    support_neg = FP + TN
+    print(f"Support (1): {round(support_neg*100,)}")
+
+
+
+
+
+def baseline(df,var):
+    ''' Method calculates the minimum odds that a model will need to beat,
+    based on the target variable and the dataframe passed in
+    '''
+    telco = ac.get_telco_data()
+    if (df[var].value_counts(normalize=True))[0] > (df[var].value_counts(normalize=True))[1]:
+        baseline = (df[var].value_counts(normalize=True))[0]
+    else: 
+        baseline = (df[var].value_counts(normalize=True))[1]
+    return baseline
+
+#example template for how to call function
+#print(f"Baseline accuracy = {round(baseline*100,2)}%") 
+
+
+
+def make_pie(df, var):
+    '''Make Pie Chart demonstrating churn as a problem'''
+
+    # set values and labels for chart
+    #yes = len(telco.churn[telco.churn == 'Yes'])
+    #no  = len(telco.churn[telco.churn == 'No'])
+    #yes = len(df.var[df.var == 'Yes'])
+    #no  = len(df.var[df.var == 'No'])
+    #values = [yes, no]
+    values = [len(df[var][df[var] == 'Yes']), len(df[var][df[var] == 'No'])] 
+    labels = ['Churned','Did not Churn' ] 
+
+    # generate and show chart
+    plt.pie(values, labels=labels, autopct='%.0f%%', colors=['#ffc3a0', '#c0d6e4'])
+    plt.title('Customers who churned.')
+    plt.show()
+
+
+def baseline_bar(var):
+    "get graph of baseline rating"
+
+    # assign values and labels
+    labels = ['Churned', 'Did not Churn']
+    values = [var*100, (1-var)*100]
+
+    # generate and display graph
+    plt.bar(labels, values, color=['#ffc3a0', '#c0d6e4'])
+    plt.title('The baseline statistic our model must beat')
+    plt.tight_layout()
+    plt.show()
+
+
+def get_t_score(df, var, target_var):
+    "get t-test for mean rating difference in tenure "
+    t, p = stats.ttest_ind(df[var][df[target_var] == 'Yes'],df[var][df[target_var] == 'No'])
+#    t, p = stats.ttest_ind(train.rating_difference[(train.upset == True)],train.rating_difference[(train.upset == False)])
+
+    return(t, p)
+#    print(f't = {t:.4f}')
+#    print(f'p = {p:.4f}')
+
+
+
+# Copy of decision tree model from best, least overfit decision tree model
+def decision_tree_model(x_train, y_train, x_test, y_test):
+    # Make the model
+    tree = DecisionTreeClassifier(max_depth=5, random_state=7)
+
+    # Fit the model (on train and only train)
+    final_model = tree.fit(x_train, y_train)
+
+    # Use the model
+    # We'll evaluate the model's performance on train, first
+    #in_sample_accuracy = tree.score(x_train, y_train)
+
+    final_model_accuracy = tree.score(x_test, y_test)
+    #y_predictions = final_model.predict(x_test)
+    #report = classification_report(y_test, y_predictions, output_dict=True)
+
+    return final_model_accuracy
+
+
+
+##### Quick and dirty copying of models from notebook. KNN of 7 and KNN of 10
+def knn10_model(x_train, y_train, x_test, y_test):
+    test_predict = {
+        'model': [],
+        'accuracy': [],
+        'true_positive_rate': [],
+        'false_positive_rate': [],
+        'true_negative_rate': [],
+        'false_negative_rate': [],
+        'precision': [],
+        'recall': [],
+        'f1_score': [],
+        'support_0': [],
+        'support_1': []
+    }
+    n = 10
+
+    knn = KNeighborsClassifier(n_neighbors=n)
+
+    knn.fit(x_train, y_train)
+
+    y_preds = knn.predict(x_test)
+
+    TN, FP, FN, TP = confusion_matrix(y_test, y_preds).ravel()
+    ALL = TP + TN + FP + FN
+
+    accuracy = (TP + TN)/ALL
+    true_positive_rate = TP/(TP+FN)
+    false_positive_rate = FP/(FP+TN)
+    true_negative_rate = TN/(TN+FP)
+    false_negative_rate = FN/(FN+TP)
+    precision = TP/(TP+FP)
+    recall = TP/(TP+FN)
+    f1_score = 2*(precision*recall)/(precision+recall)
+    support_pos = TP + FN
+    support_neg = FP + TN
+
+    test_predict['model'].append(f'knn_n_{n}')
+    test_predict['accuracy'].append(accuracy)
+    test_predict['true_positive_rate'].append(true_positive_rate)
+    test_predict['false_positive_rate'].append(false_positive_rate)
+    test_predict['true_negative_rate'].append(true_negative_rate)
+    test_predict['false_negative_rate'].append(false_negative_rate)
+    test_predict['precision'].append(precision)
+    test_predict['recall'].append(recall)
+    test_predict['f1_score'].append(f1_score)
+    test_predict['support_0'].append(support_pos)
+    test_predict['support_1'].append(support_neg)
+
+    test_predict = pd.DataFrame(test_predict).T
+
+    return test_predict.T['accuracy'][0]
+
+
+def knn7_model(x_train, y_train, x_test, y_test):
+    test_predict = {
+        'model': [],
+        'accuracy': [],
+        'true_positive_rate': [],
+        'false_positive_rate': [],
+        'true_negative_rate': [],
+        'false_negative_rate': [],
+        'precision': [],
+        'recall': [],
+        'f1_score': [],
+        'support_0': [],
+        'support_1': []
+    }
+    n = 7
+
+    knn = KNeighborsClassifier(n_neighbors=n)
+
+    knn.fit(x_train, y_train)
+
+    y_preds = knn.predict(x_test)
+
+    TN, FP, FN, TP = confusion_matrix(y_test, y_preds).ravel()
+    ALL = TP + TN + FP + FN
+
+    accuracy = (TP + TN)/ALL
+    true_positive_rate = TP/(TP+FN)
+    false_positive_rate = FP/(FP+TN)
+    true_negative_rate = TN/(TN+FP)
+    false_negative_rate = FN/(FN+TP)
+    precision = TP/(TP+FP)
+    recall = TP/(TP+FN)
+    f1_score = 2*(precision*recall)/(precision+recall)
+    support_pos = TP + FN
+    support_neg = FP + TN
+
+    test_predict['model'].append(f'knn_n_{n}')
+    test_predict['accuracy'].append(accuracy)
+    test_predict['true_positive_rate'].append(true_positive_rate)
+    test_predict['false_positive_rate'].append(false_positive_rate)
+    test_predict['true_negative_rate'].append(true_negative_rate)
+    test_predict['false_negative_rate'].append(false_negative_rate)
+    test_predict['precision'].append(precision)
+    test_predict['recall'].append(recall)
+    test_predict['f1_score'].append(f1_score)
+    test_predict['support_0'].append(support_pos)
+    test_predict['support_1'].append(support_neg)
+
+    test_predict = pd.DataFrame(test_predict).T
+
+    return test_predict.T['accuracy'][0]
+
+
+def models_bar(vars):
+    "get graph of model accuracy situated next to baseline rating"
+
+    # assign values and labels
+    labels = ['Baseline', 'Decision Tree', 'KNN7', 'KNN10']
+    models = vars
+
+    # generate and display graph
+    plt.bar(labels, models, color=['#ffc3a0', '#c0d6e4'])
+    plt.title('Chosen Models Compared to Baseline')
+    plt.tight_layout()
+    plt.show()
+
+def t_test_bar(vars):
+    "get graph of model accuracy situated next to baseline rating"
+
+    # assign values and labels
+    labels = ['Level for Significance', 'Tenure','Monthly Charges']
+    models = vars
+
+    # generate and display graph
+    plt.bar(labels, models, color=['#ffc3a0', '#c0d6e4'])
+    plt.title('Any amount over 2.015 demonstrates a relationship')
+    plt.tight_layout()
+    plt.show()
---
+++ prepare.py
@@ -0,0 +1,196 @@
+import pandas as pd
+import numpy as np
+
+from sklearn.model_selection import train_test_split
+from sklearn.impute import SimpleImputer
+
+
+
+'''
+████████╗███████╗██╗░░░░░░█████╗░░█████╗░ 
+╚══██╔══╝██╔════╝██║░░░░░██╔══██╗██╔══██╗ 
+░░░██║░░░█████╗░░██║░░░░░██║░░╚═╝██║░░██║ 
+░░░██║░░░██╔══╝░░██║░░░░░██║░░██╗██║░░██║ 
+░░░██║░░░███████╗███████╗╚█████╔╝╚█████╔╝ 
+░░░╚═╝░░░╚══════╝╚══════╝░╚════╝░░╚════╝░ 
+'''
+
+def helper():
+    pass_into_prep_telco_data = 'train, validate, model = prep_telco_data(df)'
+    pass_into_model_telco_data = 'x_train, y_train, x_validate, y_validate, x_test, y_test = model_telco_data(df)'
+    list_of_main_functions = [pass_into_prep_telco_data, pass_into_model_telco_data]
+    
+    print('Main functions:',*list_of_main_functions, sep = '\n\n')
+
+
+####################################################################################
+####################           Main Function option 1           ####################
+####################################################################################
+##########  Function to prepare the Telco Data Frame and return clean df  ##########
+####################################################################################
+
+
+def prep_telco_data(df):
+
+    '''
+    Strips Data Frame down, adding dummy variables for several categories
+    of services. It additionally removes unneeded variables. 
+    '''
+    # Drop duplicate columns
+    df.drop(columns=['payment_type_id', 'internet_service_type_id', 'contract_type_id', 'customer_id'], inplace=True)
+
+    # Drop null values stored as whitespace    
+    df['total_charges'] = df['total_charges'].str.strip()
+    df = df[df.total_charges != '']
+    
+    # Convert to correct datatype
+    df['total_charges'] = df.total_charges.astype(float)
+    
+    # Convert binary categorical variables to numeric
+    df['is_female'] = df.gender.map({'Female': 1, 'Male': 0})
+    df['has_partner'] = df.partner.map({'Yes': 1, 'No': 0})
+    df['has_dependents'] = df.dependents.map({'Yes': 1, 'No': 0})
+    df['has_phone_service'] = df.phone_service.map({'Yes': 1, 'No': 0})
+    df['has_paperless_billing'] = df.paperless_billing.map({'Yes': 1, 'No': 0})
+    df['did_churn'] = df.churn.map({'Yes': 1, 'No': 0})
+    
+    
+    # Get dummies for non-binary categorical variables
+    dummy_df = pd.get_dummies(df[['multiple_lines', \
+                              'online_security', \
+                              'online_backup', \
+                              'device_protection', \
+                              'tech_support', \
+                              'streaming_tv', \
+                              'streaming_movies', \
+                              'contract_type', \
+                              'internet_service_type', \
+                              'payment_type']], dummy_na=False, \
+                              drop_first=True)
+    
+    # Concatenate dummy dataframe to original 
+    df = pd.concat([df, dummy_df], axis=1)
+    
+    # uses split_telco_data function to develop and return train, validate, and test variables
+    train, validate, test = split_telco_data(df)
+    
+
+    #return x_train, y_train, x_validate, y_validate, x_test, y_test 
+    return train, validate, test
+
+
+#################################################################################
+####################         Main Function option 2          ####################
+#################################################################################
+##########    Function to prepare the Telco Data Frame for Modeling    ##########
+#################################################################################
+
+
+def model_telco_data(df):
+
+    '''
+    Strips Data Frame down, adding dummy variables for several categories
+    of services. It additionally removes unneeded variables. 
+    '''
+    # Drop duplicate columns
+    df.drop(columns=['payment_type_id', 'internet_service_type_id', 'contract_type_id', 'customer_id'], inplace=True)
+
+    # Drop null values stored as whitespace    
+    df['total_charges'] = df['total_charges'].str.strip()
+    df = df[df.total_charges != '']
+
+    # Convert to correct datatype
+    df['total_charges'] = df.total_charges.astype(float)
+
+    # Convert binary categorical variables to numeric
+    df['is_female'] = df.gender.map({'Female': 1, 'Male': 0})
+    df['has_partner'] = df.partner.map({'Yes': 1, 'No': 0})
+    df['has_dependents'] = df.dependents.map({'Yes': 1, 'No': 0})
+    df['has_phone_service'] = df.phone_service.map({'Yes': 1, 'No': 0})
+    df['has_paperless_billing'] = df.paperless_billing.map({'Yes': 1, 'No': 0})
+    df['did_churn'] = df.churn.map({'Yes': 1, 'No': 0})
+
+    # Get dummies for non-binary categorical variables
+    dummy_df = pd.get_dummies(df[['multiple_lines', \
+                            'online_security', \
+                            'online_backup', \
+                            'device_protection', \
+                            'tech_support', \
+                            'streaming_tv', \
+                            'streaming_movies', \
+                            'contract_type', \
+                            'internet_service_type', \
+                            'payment_type']], dummy_na=False, \
+                            drop_first=True)
+    
+    # Joins original dataframe with newly constructed dataframe using converted dummy variables
+    df = pd.concat([df, dummy_df], axis = 1)
+
+    # Creates dummy variables for customers based on which quartile their monthly charges fit
+    df['charges_lower_quartile'] = df.monthly_charges <= df.monthly_charges.quantile(.25)
+    df['charges_higher_quartile'] = df.monthly_charges >= df.monthly_charges.quantile(.75)
+    dummy_df['mid_charge1'] = df.monthly_charges < df.monthly_charges.quantile(.75)
+    dummy_df['mid_charge2'] = df.monthly_charges > df.monthly_charges.quantile(.25)
+    df['mid_charge'] = dummy_df['mid_charge1'] == dummy_df['mid_charge2']
+
+
+
+
+
+    #Drops variables which have had binary variables created to represent for machine learning 
+    df = df.drop(columns=['gender', 'partner', 'dependents', 'phone_service', 'paperless_billing',
+                        'multiple_lines', 'online_security', 'online_backup', 'device_protection', 
+                        'tech_support', 'streaming_tv', 'streaming_movies', 'contract_type', 
+                        'internet_service_type', 'payment_type', 'churn'])
+
+    #Drops variables unused for models
+    df = df.drop(columns=['monthly_charges', 'total_charges'])
+    
+    # uses split_telco_data function to develop and return train, validate, and test variables
+    train, validate, test = split_telco_data(df)
+    
+    # 
+    x_train, y_train, x_validate, y_validate, x_test, y_test = model(train, validate, test)
+
+    return train, validate, test, x_train, y_train, x_validate, y_validate, x_test, y_test 
+    
+
+################################################################################################################
+########## Function to Take in Data Frame and then Split it into train, validate and test Data Frames ##########
+################################################################################################################
+
+
+def split_telco_data(df):
+    '''
+    This function performs split on telco data, stratify churn.
+    Returns train, validate, and test dfs.
+    '''
+    train_validate, test = train_test_split(df, test_size=.2, 
+                                        random_state = 7, 
+                                        stratify=df.did_churn)
+    train, validate = train_test_split(train_validate, test_size=.3, 
+                                   random_state = 7, 
+                                   stratify=train_validate.did_churn)
+    return train, validate, test
+
+
+####################################################################################################
+########## Further Splits Data Frames into X and Y Groups for Machine Learning Algorithms ##########
+####################################################################################################
+
+
+def model(train, validate, test):
+    ''' splitting data into two groups further. One group is going to be used to guess 
+    the y outcome. The other group will know the y outcome and will be used to verify
+    the model we create '''
+
+    x_train = train.drop(columns=['did_churn'])
+    y_train = train.did_churn
+
+    x_validate = validate.drop(columns=['did_churn'])
+    y_validate = validate.did_churn
+
+    x_test = test.drop(columns=['did_churn'])
+    y_test = test.did_churn
+
+    return x_train, y_train, x_validate, y_validate, x_test, y_test 
